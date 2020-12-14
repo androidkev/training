38d776652bf394f768ba9310bb47ceb2192f8db8
androidkev <johnkevyncastro@gmail.com>
2020-12-14 16:26:19 +0800

    tictactoe

---
+++ Tictactoe/tic-tac-toe/src/app/squares/squares.component.ts
@@ -0,0 +1,12 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-squares',
+  templateUrl: './squares.component.html',
+  styleUrls: ['./squares.component.scss']
+})
+export class SquaresComponent{
+
+  @Input() value: 'X' | 'O';
+
+}
